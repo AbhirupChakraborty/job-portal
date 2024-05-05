ef6dffe4098ee07c862039dd66e853b262798521
AbhirupChakraborty <abhirup.chakraborty1998@gmail.com>
2024-05-05 20:50:19 +0530

    (Avatar add): in referral button

--- src/Components/JobCard.tsx
+++ src/Components/JobCard.tsx
@@ -2,8 +2,9 @@ import { makeStyles } from '@mui/styles';
 import Card from '@mui/material/Card';
 import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
-import { Gradient } from '@mui/icons-material';
-import { hover } from '@testing-library/user-event/dist/hover';
+import StyledBadge from '@mui/material/Badge';
+import Avatar from '@mui/material/Avatar'; 
+import Stack from '@mui/material/Stack';
 
 interface Job {
   companyName: string;
@@ -87,6 +88,12 @@ const useStyles = makeStyles({
       backgroundColor: '#77FDB3 !important',
     },
   },
+  avatar: {
+    width: '25px !important',
+    height: '25px !important',
+    wordSpacing: '5px',
+    border: '1px solid black',
+  },
   referralButton: {
     width: '100%',
     borderRadius: '6px !important',
@@ -95,7 +102,7 @@ const useStyles = makeStyles({
     '&:hover': {
       backgroundColor: '#3632fb !important',
     },
-  }
+  },
 });
 
 function convertUSDtoINR(usdAmount: number) {
@@ -107,61 +114,81 @@ function JobCard({ job }: JobCardProps) {
   const classes = useStyles();
 
   return (
-    <Card className={classes.jobCard}>
-      <div className={classes.timeLine}>⏳ Posted 10 days ago</div>
-      <div className={classes.jobInfo}>
-        <div>
-          <img src={job.logoUrl} alt="logo" className={classes.companyLogo} />
-        </div>
-        <div>
-          <Typography className={classes.companyInfo} variant="subtitle1" gutterBottom>
-            {job.companyName}
-          </Typography>
-          <Typography className={classes.companyInfo} variant="subtitle1" gutterBottom>
-            {job.jobRole} Engineer
-          </Typography>
-          <Typography className={classes.companyInfo} variant="subtitle1" gutterBottom>
-            {job.location}
-          </Typography>
-        </div>
-      </div>
+  <Card className={classes.jobCard}>
+    <div className={classes.timeLine}>⏳ Posted 10 days ago</div>
+    <div className={classes.jobInfo}>
       <div>
-        <Typography variant="body1" className={classes.salary}>
-          Estimated Salary: ₹ {convertUSDtoINR(job.minJdSalary !== null ? job.minJdSalary : 0)} - {convertUSDtoINR(job.maxJdSalary)} LPA ✅
-        </Typography>
-        <Typography variant="h6" gutterBottom>
-          About Company:
-        </Typography>
-        <Typography variant="subtitle1" gutterBottom>
-          About us
-        </Typography>
-        <p className={classes.jobDetails}>
-          <Typography className={classes.description} variant="body2">
-            <span>{job.jobDetailsFromCompany}</span>
-          </Typography>
-          <span className={classes.viewJob}> View Job </span>
-        </p>
-        {job.minExp !== null ? (
-          <Typography variant="body1">
-            Minimum Experience: 
-            <p>{job.minExp} years</p>
-          </Typography>
-        ) : (
-          <Typography variant="body1">
-            Minimum Experience:
-            <p> Not specified </p>
-          </Typography>
-        )}
-        <div>
-          <Button variant="text" className={classes.applyButton} sx={{ textTransform: 'none' }} href="https://weekday.works">
-            ⚡Easy Apply
-          </Button>
-          <Button variant="text" className={classes.referralButton} sx={{ textTransform: 'none' }} href="https://weekday.works">
-            Unlock referral asks
-          </Button>
-        </div>
+        <img src={job.logoUrl} alt="logo" className={classes.companyLogo} />
       </div>
-    </Card>
+      <div style={{ marginLeft: '16px' }}>
+        <Typography className={classes.companyInfo} sx={{ fontWeight: 'bold', color: 'grey' }} variant="subtitle1" gutterBottom>
+          {job.companyName}
+        </Typography>
+        <Typography className={classes.companyInfo} sx={{ fontWeight: 400 }} variant="subtitle1" gutterBottom>
+          {job.jobRole} Engineer
+        </Typography>
+        <Typography className={classes.companyInfo} sx={{ fontWeight: 500 }} variant="subtitle1" gutterBottom>
+          {job.location}
+        </Typography>
+      </div>
+    </div>
+    <div style={{ marginTop: '16px' }}>
+      <Typography variant="body1" className={classes.salary}>
+        Estimated Salary: ₹ {convertUSDtoINR(job.minJdSalary !== null ? job.minJdSalary : 0)} - {convertUSDtoINR(job.maxJdSalary)} LPA ✅
+      </Typography>
+      <Typography variant="h6" sx={{ fontWeight: 500, color: '#5A5A5A', marginTop: '16px' }} gutterBottom>
+        About Company:
+      </Typography>
+      <Typography sx={{ fontWeight: 'bold' }} variant="subtitle1" gutterBottom>
+        About us
+      </Typography>
+      <p className={classes.jobDetails}>
+        <Typography className={classes.description} variant="body2">
+          <span>{job.jobDetailsFromCompany}</span>
+        </Typography>
+        <span className={classes.viewJob}> View Job </span>
+      </p>
+      {job.minExp !== null ? (
+      <>
+        <Typography variant="body1" sx={{ fontWeight: 'bold', color: 'grey', marginTop: '16px' }}>
+          Minimum Experience:
+        </Typography>
+        <p>{job.minExp} years</p>
+      </>
+      ) : (
+      <>
+        <Typography variant="body1" sx={{ fontWeight: 'bold', color: 'grey', marginTop: '16px' }}>
+          Minimum Experience:
+        </Typography>
+        <p> Not specified </p>
+      </>
+      )}
+    <div>
+      <Button variant="text" className={classes.applyButton} sx={{ fontWeight: '700', textTransform: 'none' }} href="https://weekday.works">
+        ⚡Easy Apply
+      </Button>
+      <Button variant="text" className={classes.referralButton} sx={{ textTransform: 'none' }} href="https://weekday.works">
+        <Stack direction="row" spacing={2}>
+          <StyledBadge
+            overlap="circular"
+            anchorOrigin={{ vertical: 'bottom', horizontal: 'right' }}
+            variant="dot"
+          >
+            <Avatar className={classes.avatar} alt="Remy Sharp" src="/static/images/avatar/1.jpg" />
+          </StyledBadge>
+          <StyledBadge
+            overlap="circular"
+            anchorOrigin={{ vertical: 'bottom', horizontal: 'right' }}
+            variant="dot"
+          >
+            <Avatar className={classes.avatar} alt="Travis Howard" src="/static/images/avatar/2.jpg" />
+          </StyledBadge>
+        </Stack>
+        Unlock referral asks
+      </Button>
+    </div>
+    </div>
+  </Card>
   );
 }
 
